9b8c26897bc45bfd00702ff5e78e44022e6dc68b
1101137127 <s42861@yahoo.com.tw>
2015-05-01 19:16:23 +0800

    Homework

---
+++ KuasCore/Services/Impl/EmployeeService.cs
@@ -0,0 +1,45 @@
+﻿using KuasCore.Dao;
+using KuasCore.Dao.Impl;
+using KuasCore.Models;
+using System.Collections.Generic;
+
+namespace KuasCore.Services.Impl
+{
+    public class EmployeeService : IEmployeeService
+    {
+
+        public IEmployeeDao EmployeeDao { get; set; }
+
+        public void AddEmployee(Employee employee)
+        {
+            EmployeeDao.AddEmployee(employee);
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            EmployeeDao.UpdateEmployee(employee);
+        }
+
+        public void DeleteEmployee(Employee employee)
+        {
+            employee = EmployeeDao.GetEmployeeByName(employee.Id);
+
+            if (employee != null)
+            {
+                EmployeeDao.DeleteEmployee(employee);
+            }
+        }
+
+        public IList<Employee> GetAllEmployees()
+        {
+            return EmployeeDao.GetAllEmployees();
+        }
+
+        public Employee GetEmployeeByName(string name)
+        {
+            return EmployeeDao.GetEmployeeByName(name);
+        }
+
+    }
+
+}
---
+++ MyWebApp/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using KuasCore.Models;
+using KuasCore.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyWebApp.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEmployeeService EmployeeService { get; set; }
+
+        [HttpGet]
+        public IList<Employee> GetAllEmployees()
+        {
+            return EmployeeService.GetAllEmployees();
+        }
+
+        [HttpGet]
+        [ActionName("byName")]
+        public Employee GetEmployeeByName(string name)
+        {
+            return EmployeeService.GetEmployeeByName(name);
+
+        }
+
+    }
+}
